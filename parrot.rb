9dd7b466bf48fa7fd3839b67e1c7d2b1f0c0fd97
Nicholas Patel <np4795@yahoo.com>
2020-02-11 01:04:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 def parrot(name)
   puts "Squawk!"
  return "Squawk!"
- puts parrot("Pretty bird!")
- return parrot("Pretty bird!")
+ puts parrot#{"Pretty bird!"}
+ return parrot#{"Pretty bird!"}
 end
 parrot
